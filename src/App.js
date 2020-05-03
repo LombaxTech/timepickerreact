feec718383d24cc31644ba8596ffabb10037883b
Rakib Khan <rakibkhan@live.co.uk>
2020-05-03 02:35:15 +0100

    it was the bloody imports

--- src/App.js
+++ src/App.js
@@ -4,58 +4,40 @@ import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
 
-import setHours from "date-fns/setMinutes";
-import setMinutes from "date-fns/setMinutes";
+// ! importing like this messes up includeTimes for some reason
+// import setHours from "date-fns/setMinutes";
+// import setMinutes from "date-fns/setMinutes";
 
-
-// function App() {
-//   const [startDate, setStartDate] = useState(new Date());
-
-//   return (
-//     <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
-//   );
-// }
-
-// const App = () => {
-
-//   const [startDate, setStartDate] = useState(
-//     setHours(setMinutes(new Date(), 30), 16)
-//   );
-
-//   return (
-//     <DatePicker
-//       selected={startDate}
-//       onChange={date => setStartDate(date)}
-//       showTimeSelect
-//       excludeTimes={[
-//         setHours(setMinutes(new Date(), 0), 17),
-//         setHours(setMinutes(new Date(), 30), 18),
-//         setHours(setMinutes(new Date(), 30), 19),
-//         setHours(setMinutes(new Date(), 30), 17)
-//       ]}
-//       dateFormat="MMMM d, yyyy h:mm aa"
-//     />
-//   );
-// };
+// ! importing like this does work
+import { setHours, setMinutes, addDays, subDays } from "date-fns";
 
 const App = () => {
+
   const [startDate, setStartDate] = useState(
     setHours(setMinutes(new Date(), 30), 16)
   );
+
   return (
-    <DatePicker
-      selected={startDate}
-      onChange={date => setStartDate(date)}
-      showTimeSelect
-      includeTimes={[
-        setHours(setMinutes(new Date(), 0), 17),
-        setHours(setMinutes(new Date(), 30), 18),
-        setHours(setMinutes(new Date(), 30), 19),
-        setHours(setMinutes(new Date(), 30), 17)
-      ]}
-      dateFormat="MMMM d, yyyy h:mm aa"
-    />
+    <div>
+      <DatePicker
+        selected={startDate}
+        onChange={date => setStartDate(date)}
+        showTimeSelect
+        minDate={subDays(new Date(), 0)}
+        maxDate={addDays(new Date(), 13)}
+        excludeTimes={[
+          setHours(setMinutes(new Date(), 0), 17),
+          setHours(setMinutes(new Date(), 0), 18),
+          setHours(setMinutes(new Date(), 0), 19),
+          setHours(setMinutes(new Date(), 0), 17)
+        ]}
+        dateFormat="MMMM d, yyyy h:mm aa"
+      />
+      {console.log(startDate)}
+      {console.log()}
+    </div>
   );
-};
+
+}
 
 export default App;
