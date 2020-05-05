6e308bf69e6545154d3346a7fa85cd5daccc0ef9
Rakib Khan <rakibkhan@live.co.uk>
2020-05-05 03:01:31 +0100

    .

---
+++ src/data/unavailableTimes.js
@@ -0,0 +1,17 @@
+const unavailableTimes = {
+    one: [9, 10, 11],
+    two: [
+
+    ],
+    three: [
+
+    ],
+    four: [
+
+    ],
+    five: [
+
+    ]
+}
+
+export default unavailableTimes;
--- src/secondTimePicker.js
+++ src/secondTimePicker.js
@@ -3,6 +3,16 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { setHours, setMinutes, addDays, subDays } from "date-fns";
 import bookings from './data/bookings'
+import unavailableTimes from './data/unavailableTimes'
+
+let nightBookings = [
+    new Date(2020, 4, 10, 1),
+    new Date(2020, 4, 10, 2),
+    new Date(2020, 4, 10, 3),
+    new Date(2020, 4, 10, 4),
+    new Date(2020, 4, 10, 5),
+    new Date(2020, 4, 10, 6)
+]
 
 let todaysBookings = bookings.filter(booking => {
     return (booking.timeDetails.day === new Date().getDate())
@@ -24,17 +34,44 @@ const SecondTimePicker = () => {
         console.log(`Year is: ${startDate.getFullYear()}, Month is: ${startDate.getMonth()}, Day is: ${startDate.getDate()}, Hour is: ${startDate.getHours()}`)
     }
 
-    const updateAvailableTimes = date => {
-        // console.log(date)
+    const isWeekday = date => {
+        return date.getDay() != 0 && date.getDay() != 6
+    }
+
+    const updateBookedTimes = date => {
+
         // * 
         // * bookedHours = getBookedHours(date)
-        // * setBookedTimes(bookedHours) 
+        // * setBookedTimes(bookedHours)
+
+        // * SETTING TUTOR CHOSEN UNAVAILABLE TIMES
+
+        let day = date.getDay();
+
+        switch (day) {
+            case 1:
+                day = 'one'
+                break;
+            case 2:
+                day = 'two'
+                break;
+            case 3:
+                day = 'three'
+                break;
+            case 4:
+                day = 'four'
+                break;
+            case 5:
+                day = 'five'
+                break;
+            default:
+                console.log('error')
+                break;
+        }
+
+        console.log(day)
 
         setBookedTimes([
-            // setHours(setMinutes(new Date(), 0), 1),
-            // setHours(setMinutes(new Date(), 0), 2),
-            // setHours(setMinutes(new Date(), 0), 3),
-            // setHours(setMinutes(new Date(), 0), 4)
             new Date(2020, 3, 4, 13),
             new Date(2020, 3, 4, 17)
         ])
@@ -43,22 +80,24 @@ const SecondTimePicker = () => {
     return (
         <div>
             <DatePicker
+                filterDate={isWeekday}
                 timeIntervals={60}
                 selected={startDate}
                 onChange={date => {
                     setStartDate(date);
-                    updateAvailableTimes(date)
+                    updateBookedTimes(date)
                 }}
                 showTimeSelect
                 minDate={subDays(new Date(), 0)}
                 maxDate={addDays(new Date(), 13)}
-                excludeTimes={bookedTimes}
+                excludeTimes={[...nightBookings, ...bookedTimes]}
                 dateFormat="MMMM d, yyyy h:mm aa"
             />
             <button
             >
                 Book Lesson
             </button>
+            {console.log(unavailableTimes.one)}
         </div>
     );
 
